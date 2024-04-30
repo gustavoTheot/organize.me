b77c5336f97dd776e579f2be89eb2e4dedaea520
Gustavo Theotonio <gustavotheotonio46@gmail.com>
2024-04-30 16:20:37 -0300

    feat-styled-spreadsheet

--- frontend/src/pages/spreadsheet/index.tsx
+++ frontend/src/pages/spreadsheet/index.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
 import { api } from "../../api/axios";
 import { Header } from "../../component/header";
-import { Main, Nav } from "./style";
+import { Filter, Main, Nav } from "./style";
 import { formatCurrency } from "../../util/formatValue";
 import { formatDate } from "../../util/formatDate";
+import { Cardholder, CaretDown, TrendDown, TrendUp, Wallet } from "@phosphor-icons/react";
 
 export function SpredSheet() {
     const currentMonth = new Date().getMonth()+1
@@ -35,45 +36,64 @@ export function SpredSheet() {
         <Header/>
             
         <Nav>
+            <Filter>
+                <label htmlFor="">Mês</label>
+                <select onChange={handleChange} value={month}>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+                    <option value="11">11</option>
+                    <option value="12">12</option>
+                </select>
+            </Filter>
+
             <ul>
                 <li>
-                    <span>Saldo atual</span>
-                    {'R$ 100,00'}
+                    <div>
+                        <span>Saldo atual</span>
+                        {'R$ 100,00'}
+                    </div>
+                   
+                    <Wallet size={24}/>
                 </li>
                 <li>
-                    <span>Receita</span>
-                    {'R$ '}
+                    <div>
+                        <span>Receita</span>
+                        {'R$ '}
+                    </div>
+                    
+                    <TrendUp size={24} />
                 </li>
                 <li>
-                    <span>Despesa</span>
-                    {'R$ '}
+                    <div>
+                        <span>Despesa</span>
+                        {'R$ '}
+                    </div>
+
+                    <TrendDown size={24} />
                 </li>
                 <li>
-                    <span>Cartão de crédito</span>
-                    {'R$ '}
+                    <div>
+                        <span>Cartão de crédito</span>
+                        {'R$ '}
+                    </div>
+
+                    <Cardholder size={24} />
                 </li>
             </ul>
         </Nav>
         <Main>
-            <label htmlFor="">Mês</label>
-            <select onChange={handleChange} value={month}>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
-                <option value="11">11</option>
-                <option value="12">12</option>
-            </select>
             <table>
                 <thead>
                     <tr>
-                    <   th>Local</th>
+                        <th>Local</th>
                         <th>Valor gasto</th>
                         <th>Tipo</th>
                         <th>Forma de pagamento</th>
--- frontend/src/pages/spreadsheet/style.ts
+++ frontend/src/pages/spreadsheet/style.ts
@@ -1,7 +1,93 @@
 import styled from "styled-components";
 
+export const Filter = styled.div`
+    margin-top: 5rem;
+    margin-bottom: 1rem;
+
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+
+    select{
+        margin-left: 1rem;
+        border: none;
+    }
+`
+
 export const Nav = styled.nav`
+
+    ul{ 
+        display: flex;
+        gap: 1rem;
+
+        li{
+            list-style: none;
+            width: 12rem;
+            height: 5rem;
+
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            gap: 1rem;
+            padding-left: 1rem;
+            border-radius: 8px;
+
+            &:first-child, &:last-child{
+                color: ${props => props.theme['ice-100']};
+                background-color: ${props => props.theme['blue-400']};
+            };
+
+            &:nth-child(2), &:nth-child(3){
+                border: 1px solid ${props => props.theme['grey-200']};
+            };
+
+            div{
+                display: flex;
+                flex-direction: column;
+            }
+
+            span{
+                font-size: 0.75rem;
+            }
+        }
+    }
 `
 
 export const Main = styled.main`
+    margin-top: 2rem;
+
+    table{
+        width: 50rem;
+
+        thead{
+            tr{
+                th{
+                    height: 2rem;
+
+                    text-align: start;
+
+                    &:first-child{
+                        width: 12rem;
+                    };
+
+                    &:nth-child(4){
+                        width: 12rem;
+                    }
+
+                    &:last-child{
+                        width: 7.8rem;
+                    }
+                } 
+            }
+        }
+
+        tbody{
+            tr{
+                td{
+                    height: 2rem;
+                }
+            }
+        }
+    }
+
 `
